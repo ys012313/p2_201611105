2a89eeac5fd201e134c94de8b423d893129bb38e
limjeongmin <jml8740@naver.com>
2016-03-29 23:20:37 +0900

    week4 assignment
    week4 assignment

---
+++ w4Main.py
@@ -0,0 +1,15 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+def makeSwirlSquare(size,angle,bigger,sides):
+    for i in range(0,sides):
+        if(i%2):
+            size=size+bigger
+            t1.fd(size)
+            t1.right(angle)
+        else:
+            size=size
+            t1.fd(size)
+            t1.right(angle)
+makeSwirlSquare(10,90,10,20)
+wn.exitonclick()
