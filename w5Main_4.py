e607624bc5cd1b52cb11ddbcac0529946f0795ea
p2-p1_201611105 정수환 <jshking7177@naver.com>
2016-04-03 23:04:11 +0900

    Added files via upload

---
+++ w5Main_4.py
@@ -0,0 +1,13 @@
+﻿userA=raw_input('userA input rock, sicssors, paper : ')
+userB=raw_input('userB input rock, sicssors, paper : ')
+if userA!=userB:
+    if userA=='paper' and userB=='rock':
+       print 'UserA win'
+    elif userA=='rock' and userB=='scissors':
+       print 'UserA win'
+    elif userA=='sicssors' and userB=='rock':
+       print 'UserA win'
+    else:
+       print 'UserB win'
+elif userA==userB:
+            print "Draw"
