fecd2c2bc2222c758d9424a8190988fc62d1ae50
p2-p1_201611105 정수환 <jshking7177@naver.com>
2016-04-04 18:06:39 +0900

    w5Main8

---
+++ w5Main8.py
@@ -0,0 +1,13 @@
+﻿height=input ("input user height (cm): ")
+weight=input ("input user weight (kg): ")
+print "%.1f" %height, "%.1f" %weight
+BMI=weight*10000/(height*height)
+print "%.1f" %BMI
+if BMI<=18.5:
+    print 'Low weight'
+elif 18.5< BMI <25:
+    print 'Normal weight'
+elif 25<= BMI <30:
+    print 'Over weight'
+else:
+    print 'Obesity'
