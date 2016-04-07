bad74546e11448128206407b6ff742ae88db8681
p2-p1_201611105 정수환 <jshking7177@naver.com>
2016-04-07 17:26:19 +0900

    Added files via upload

---
+++ w6Main_9.py
@@ -0,0 +1,19 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def lab6():
+     year=raw_input("input year: ")
+     year=int(year)
+     if(year%4==0):
+         print "Leap year"
+     else:
+         print "Not Leap year"
+
+
+def main():
+     lab6()
+
+
+ main()
+t1.exitonclick()
