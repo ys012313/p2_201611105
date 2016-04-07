43b9aaf378ada0866136183697c15ff42f9e175a
p2-p1_201611105 정수환 <jshking7177@naver.com>
2016-04-08 02:38:07 +0900

    high/low game(업다운 게임)

---
+++ w6Main_11.py
@@ -0,0 +1,22 @@
+﻿import random
+guessesTaken = 0
+number = random.randint(1,50)
+print('I am thinking of a number between 1 and 50.')
+I am thinking of a number between 1 and 50.
+while guessesTaken < 100:
+     print('Take a guess.')
+     guess = input()
+     guess = int(guess)
+     guessesTaken = guessesTaken + 1
+     if guess < number:
+         print('your guess is too low.')
+     if guess > number:
+         print('your guess is too high.')
+     if guess==number:
+         break
+if guess== number:
+     guessesTaken = str(guessesTaken)
+     print('You did Good!'+' You guessed my number in'+ ' guessesTaken'+'guesses!')
+if guess !=number:
+     number = str(number)
+     print('No. the number that I was thinking of was '+number)
