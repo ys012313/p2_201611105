47a8b8b9099b6e40bc657b54b3116f6240b01c51
p2-p1_201611105 정수환 <jshking7177@naver.com>
2016-04-11 21:40:32 +0900

    1~1000 4로 나뉘어지고 5로 안나눠지는 수 합

---
+++ w6Main(4,not5).py
@@ -0,0 +1,25 @@
+﻿x=list()
+
+def sumLIST(x):
+    x=list()
+    for i in range(0,1000):
+        if i%4==0 and not i%5==0:
+            x.append(i)
+    sum=0
+    for i in range(0,len(x)):
+        sum+=x[i]
+    return sum
+
+
+def lab6():
+    print "GOOD"
+    total=sumLIST(x)
+    print total
+
+
+def main():
+    lab6()
+
+
+main()
+input()
