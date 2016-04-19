bfad9a9387f81bfed264c0874b5763b821720428
p2-p1_201611105 정수환 <jshking7177@naver.com>
2016-04-19 21:47:04 +0900

    dict 사용 과제

---
+++ w7_1main.py
@@ -0,0 +1,24 @@
+﻿import turtle
+
+wn=turtle.Screen()
+
+t1=turtle.Turtle()
+
+d=dict()
+
+d=({1:(0,0),2:(25,0),3:(25,25),4:(0,25),5:(0,0)})
+
+
+
+def lab7():
+	for i in range(1,6):
+		t1.setpos(d[i])
+
+
+def main():
+	lab7()
+
+
+main()
+
+wn.exitonclick()
