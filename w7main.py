bd29c2d20fce6d3f6955099315027c3b02ff3ea3
p2-p1_201611105 정수환 <jshking7177@naver.com>
2016-04-19 21:44:52 +0900

    dict 사용

---
+++ w7main.py
@@ -0,0 +1,26 @@
+﻿import turtle   
+wn = turtle.Screen()   
+t1= turtle.Turtle()   
+   
+def drawSquareAt(size,pos):   
+    tracks=list()   
+    t1.penup()   
+    t1.setpos(pos)   
+    t1.pendown()   
+    for i in range(0,4):   
+        tracks.append(t1,pos())   
+        t1.fd(size)   
+        t1.right(90)   
+    return tracks   
+  
+  
+def lab7():   
+    mytracks=drawSquareAt(30,(40,40))   
+    print mytracks  
+     
+def main():   
+    lab7()   
+if __name__=="__main__":    
+    main()
+
+wn.exitonclick()    
