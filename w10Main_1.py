5937da6bd41e8aa2cd55e6725c8a6fa53da1f8f2
p2-p1_201611105 정수환 <jshking7177@naver.com>
2016-05-10 23:02:20 +0900

    우유 사용한 것/ 영어,수학 성적 분석/ Let it be 가사

---
+++ w10Main_1.py
@@ -0,0 +1,17 @@
+﻿def Milk():
+    allData=[ 
+ 	    ["Coffee","Water","Milk","Icecream"], 
+ 	    ["Espresso","No","No","No"], 
+ 	    ["Long Black","Yes","No","No"], 
+ 	    ["Flat White","No","Yes","No"], 
+ 	    ["Cappuccino","No","Yes","No"], 
+ 	    ["Affogatto","No","No","Yes"] 
+ 	] 
+    Data = allData[1:]
+    milkcount=0
+    for i in Data:
+        if i[2].upper()=='YES':
+            milkcount=milkcount+1
+    print "The percent of milk is",(float(milkcount)/float(len(Data)))*100,"%"
+
+Milk()
---
+++ w10Main_2.py
@@ -0,0 +1,23 @@
+﻿def Grade(): 
+     score = [ 
+ ["English", 100], 
+ ["Math", 200], 
+ ["English", 200], 
+ ["Math", 200], 
+ ["English", 100], 
+ ["Math", 300] 
+ ] 
+     EnScore = 0 
+     MaScore = 0 
+     for i in range(0,len(score)): 
+         if score[i][0]=="English": 
+             EnScore=EnScore+score[i][1] 
+     for i in range(0,len(score)): 
+         if score[i][0]=="Math": 
+             MaScore=MaScore+score[i][1] 
+ 
+ 
+     print "The EnglishScore Sum is", EnScore,"and The MathScore Sum is", MaScore 
+     print "The EnglishScore Average is about",EnScore/3,"and The MathScore Average is about",MaScore/3 
+
+Grade()
