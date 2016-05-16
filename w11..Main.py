3a88a122de6cd5a44f07383b240fc23ea4d74efe
p2-p1_201611105 정수환 <jshking7177@naver.com>
2016-05-17 04:51:29 +0900

    사각형

---
+++ w11..Main.py
@@ -0,0 +1,42 @@
+﻿import turtle 
+wn=turtle.Screen() 
+t1=turtle.Turtle() 
+def isInRectangle(curpos,coord): 
+    xs=coord[0][0] 
+    xe=coord[1][0] 
+    ys=coord[0][1] 
+    ye=coord[1][1] 
+    if (xs <= curpos[0] <= xe and ys <= curpos[1] <=ye): 
+        print ("True") 
+    else: 
+        print ("False") 
+coord=[(100,100),(150,150)] 
+t1.penup() 
+t1.goto(100,100) 
+t1.pendown() 
+for i in range(4): 
+	t1.fd(50) 
+	t1.lt(90) 
+t1.penup() 
+t1.home() 
+def keyup(): 
+    t1.fd(30) 
+    curpos=t1.pos() 
+    isInRectangle(curpos,coord) 
+wn.onkey(keyup,"Up") 
+def keyleft(): 
+    t1.left(90) 
+def keyright(): 
+    t1.right(90) 
+def keydown(): 
+    t1.back(30) 
+    curpos=t1.pos() 
+    isInRectangle(curpos,coord) 
+wn.onkey(keyleft,"Left") 
+wn.onkey(keyright,"Right") 
+wn.onkey(keydown,"Down") 
+wn.listen() 
+
+ 
+wn.exitonclick()  
+
