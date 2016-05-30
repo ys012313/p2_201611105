8036aad1347f0cae006ebefbd16ca0b56d5fa391
p2-p1_201611105 정수환 <jshking7177@naver.com>
2016-05-31 05:03:44 +0900

    5.30 과제 제출

---
+++ w13Main_1.py
@@ -0,0 +1,17 @@
+﻿ 
+def file():
+    try: 
+        fin1=open('python.txt','a') 
+        fin2=open('outputNum.txt','r') 
+        for line in fin2: 
+            fin1.write(line) 
+        fin1.close() 
+        fin2.close() 
+    except Exception as e: 
+        print(e) 
+
+def lab13():
+    file()
+    
+def main():
+    lab13()
---
+++ w13Main_2.py
@@ -0,0 +1,34 @@
+﻿import os 
+import turtle 
+wn=turtle.Screen() 
+t1=turtle.Turtle()
+aFile=input("Filename")
+def getCoordsFromFile(aFile): 
+    fin=open(aFile,'r') 
+    coords=[] 
+    for line in fin: 
+        line1=line.split(',') 
+        aRect=[(line1[0],line1[1]),(line1[2],line1[3].strip())] 
+        coords.append(aRect) 
+    fin.close() 
+    return coords
+coords=getCoordsFromFile(aFile)
+
+ 
+def drawSquareWithCoords(coords): 
+	t1.penup() 
+	t1.goto(coords[0]) 
+	t1.pendown() 
+	t1.goto(coords[1][0], coords[0][1]) 
+	t1.goto(coords[1]) 
+	t1.goto(coords[0][0], coords[1][1]) 
+	t1.setpos(coords[0]) 
+	 
+def lab13() : 
+	coords=getCoordsFromFile('coords.txt') 
+	drawSquareWithCoords(coords) 
+
+def main():
+    lab13
+    
+wn.exitonclick() 
