fe52684fe5c4ddfabc4bdf03554e89badb4d4902
p2-p1_201611105 정수환 <jshking7177@naver.com>
2016-06-02 02:02:09 +0900

    6.1 과제

---
+++ w14main.py
@@ -0,0 +1,21 @@
+﻿def lab14():
+    class Dog(object):
+        def talk(self):
+            print "wal! wal!"
+            
+    class ShihtzuDog(Dog):
+        def talk(self):
+            print "si~ si~"
+            
+    class Maltese(Dog):
+        def talk(self):
+            print "mal~? mal!!"
+    
+    mydog=Dog()
+    mydogShihtzu=ShihtzuDog()
+    mydogMaltese=Maltese()
+    mydog.talk()
+    mydogShihtzu.talk()
+    mydogMaltese.talk()
+    
+lab14()
